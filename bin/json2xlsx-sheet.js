badd01248ccacde8fd4803e6cb439f2969576a47
Frederic Gascon <frederic.gascon@gmail.com>
2015-03-31 14:10:03 -0400

    changed name to json2xlsx-sheet

--- bin/json2xlsx-sheet.js
+++ bin/json2xlsx-sheet.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 var convertStream = require('../lib/convert-stream');
 
-process.stdin.pipe(convertStream()).pipe(process.stdout);
+process.stdin.pipe(convertStream(process.argv[2])).pipe(process.stdout);
